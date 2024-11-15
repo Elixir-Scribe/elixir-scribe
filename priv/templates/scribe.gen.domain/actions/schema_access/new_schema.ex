90fa9c30a245d245511816e6d15be2410547ddd2
Exadra37 <exadra37@gmail.com>
2024-11-15 11:15:40 +0000

    --wip-- [skip ci]

--- priv/templates/scribe.gen.domain/actions/schema_access/new_schema.ex
+++ priv/templates/scribe.gen.domain/actions/schema_access/new_schema.ex
@@ -1,5 +1,5 @@
   alias <%= inspect contract.schema.module %>
 
   def <%= action_first_word %>(attrs \\ %{}) when is_map(attrs) do
-    <%= inspect contract.schema.alias %>.changeset(%<%= contract.schema.human_singular %>{}, attrs)
+    <%= inspect contract.schema.alias %>.changeset(%<%= inspect contract.schema.alias %>{}, attrs)
   end
