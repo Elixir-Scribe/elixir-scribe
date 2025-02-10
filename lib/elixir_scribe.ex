f4ba2482ffc6794286b7da4a046dd2bb24ac292c
Exadra37 <exadra37@gmail.com>
2025-02-10 20:45:09 +0000

    --wip-- [skip ci]

--- lib/elixir_scribe.ex
+++ lib/elixir_scribe.ex
@@ -2,22 +2,16 @@ defmodule ElixirScribe do
   @moduledoc """
   The Elixir Scribe Generator configuration and defaults.
 
-  All functions in this Module **MUST** be considered private and only used internally.
+  Some of the defaults will be possible to customize via your app configuration:
 
-  Some of the defaults will be possible to customize via your app configuration.
+  * [Configure Default Resource Actions](#module-configure-default-resource-actions) - Provide your own list of built-in default actions that will be used each time you invoke one of the generators.
+  * [Configure Default Resource Actions Aliases](#module-configure-resource-actions-aliases) - Use aliases to rename the built-in default actions to your preferred names.
+  * [Configure Default Source Code Templates](#module-configure-default-source-code-templates) - Provide your own source code templates to be used by the Elixir Scribe generators
 
+  > #### Private Module {: .warning}
+  > All functions in this module lack documentation because they **MUST** be considered private and used internally only.
 
-  ## Defaults
-
-  ### Templates Paths
-
-  The paths to look for template files for generators defaults to checking the current app's `priv` directory, and falls back to Elixir Scribe and Phoenix's `priv` directory:
-
-  * `priv/templates/*`
-  * `elixir_scribe/priv/templates/*`
-  * `phoenix/priv/templates/*`
-
-  ### Default Actions
+  ## Configure Default Resource Actions
 
   The default actions supported by `scribe.gen.domain` and `scribe.gen.html`:
 
@@ -29,43 +23,32 @@ defmodule ElixirScribe do
   * `update` - Updates an existing Resource
   * `delete` - Deletes an existing Resource
 
+  For example, for an API you can discard the default resource actions `new` and `edit`:
 
-  ## Configuration
+  ```elixir
+  config :elixir_scribe,
+    default_resource_actions: ["list", "read", "create", "update", "delete"]
+  ```
 
-  In your app configuration you can:
+  Or, maybe you want to use the default resource actions, plus some other ones that you always need when creating a new resource:
 
-  * [Configure Default Actions](#module-configure-default-actions) - Provide your own list of built-in default actions that will be used each time you invoke one of the generators.
-  * [Configure Default Actions Aliases](#module-configure-default-actions-aliases) - Use aliases to rename the built-in default actions to your preferred names.
-  * [Configure Custom Default Actions](#module-configure-custom-default-actions) - Provide your own custom action names, that wil use the default template or one provided by your app.
+  ```elixir
+  config :elixir_scribe,
+    default_resource_actions: ["import", "export", "list", "new", "read", "edit", "create", "update", "delete"]
+  ```
 
-  ### Configure Default Actions
+  > #### Custom Resource Actions Order {: .error}
+  > Order of actions **MATTERS**, otherwise routes will not work as expected.
+  >
+  > Note how `import` and `export` were added to the begin of the list to ensure they are matched correctly by the router when serving the `GET` request.
 
-  > #### Custom Default Actions {: .info}
-  > Any custom action name provided via the configuration will be mapped to a default template, unless you provide your custom template(s) for it at the correct path in your app `priv/templates/*`.
+  > #### Custom Resource Actions {: .warning}
+  > Any custom action name provided via the configuration will be mapped to a default template, unless you provide your custom template(s) for it at the correct path in your app `priv/templates/*`. Check how to customize the templates at [Configure Default Source Code Templates](#module-configure-default-source-code-templates).
   >
   > This custom action names will be added to your router as `GET` requests. You need to customize the router as needed.
 
-  > #### Default Actions Order {: .error}
-  > Order of actions **MATTERS**, otherwise routes will not work as expected.
 
-
-  For example, for an API you can  discard the default actions `new` and `edit`:
-
-  ```elixir
-  config :elixir_scribe,
-    default_actions: ["list", "read", "create", "update", "delete"]
-  ```
-
-  Or, maybe you want to use the default actions, plus some other ones that you always need when creating a new resource:
-
-  ```elixir
-  config :elixir_scribe,
-    default_actions: ["import", "export", "list", "new", "read", "edit", "create", "update", "delete"]
-  ```
-
-  Note how `import` and `export` were added to the begin of the list to ensure they are matched correctly by the router when serving the `GET` request.
-
-  ### Configure Default Actions Aliases
+  ## Configure Resource Actions Aliases
 
   In your app configuration for `:elixir_scribe` you can map any of the built-in default actions to actions names of your preference, which it's the same as renaming them.
 
@@ -79,9 +62,17 @@ defmodule ElixirScribe do
       }
   ```
 
-  ### Configure Custom Default Actions
+  ## Configure Default Source Code Templates
 
-  When using custom actions the generators can be configured to use your own templates for code generation instead of falling back to a default template.
+  The paths to look for template files for generators defaults to checking the current app's `priv` directory, and falls back to Elixir Scribe and Phoenix's `priv` directory:
+
+  * `priv/templates/*`
+  * `elixir_scribe/priv/templates/*`
+  * `phoenix/priv/templates/*`
+
+  Provide your own templates to customize the functionality offered by the built-in ones by copying them to your `priv/templates/*` directory, and then modify them as you see fit.
+
+  For example, when using custom resource actions, the Elixir Scribe generators will default to generating code that raises an error with a message alerting you that the logic hasn’t been implemented yet. You can avoid this by providing your own custom source code template for each of your custom actions.
 
   """
 
@@ -143,17 +134,15 @@ defmodule ElixirScribe do
 
   # @IMPORTANT: Order of actions MATTERS, otherwise routes will not work as
   #  expected. Don't allow override from config, but allow to set aliases.
-  @actions ["list", "new", "read", "edit", "create", "update", "delete"]
+  @default_resource_actions ["list", "new", "read", "edit", "create", "update", "delete"]
   @doc false
-  def resource_actions(), do: @actions
+  def resource_actions() do
+    Application.get_env(:elixir_scribe, :default_resource_actions, @default_resource_actions)
+  end
 
   @doc false
   def resource_actions_aliases() do
-    # @TODO Get from configuration.
-
-    %{
-      # "default_action" => "alias"
-    }
+    Application.get_env(:elixir_scribe, :resource_actions_aliases, %{})
   end
 
   @doc false
@@ -163,12 +152,14 @@ defmodule ElixirScribe do
 
   @resource_html_actions ["read", "new", "edit", "list"]
   @doc false
-  def resource_html_actions(), do: @resource_html_actions
+  def resource_html_actions() do
+    Application.get_env(:elixir_scribe, :resource_html_actions, @resource_html_actions)
+  end
 
+  @resource_plural_actions ["index", "list"]
   @doc false
   def resource_plural_actions() do
-    # @TODO Allow to override from configuration
-    ["index", "list"]
+    Application.get_env(:elixir_scribe, :resource_plural_actions, @resource_plural_actions)
   end
 
   @doc false
